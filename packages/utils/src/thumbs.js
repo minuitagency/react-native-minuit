58b6bc3c62f7eb7bf9c1a6557a2b010ea92aee02
Logan Lgr Jumeau <brumaireuh@gmail.com>
2022-01-03 20:18:03 +0100

    Des retours

--- packages/utils/src/thumbs.js
+++ packages/utils/src/thumbs.js
@@ -1,4 +1,7 @@
 export const getThumb = (url, size = 400) => {
-  if ([400, 800].includes(size)) return url.replace('.jpeg', `_${size}.jpeg`);
-  else return url;
+  if ([400, 800].includes(size)) {
+    return url.replace('.jpeg', `_${size}.jpeg`);
+  } else {
+    return url;
+  }
 };
