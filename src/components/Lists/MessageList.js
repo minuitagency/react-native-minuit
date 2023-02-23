a10deed7f68c48fc6042a4c75e13ba807c648dc2
Thomas <thomas@minuit.agency>
2023-02-23 12:53:56 +0100

    fix MessageList component

--- src/components/Lists/MessageList.js
+++ src/components/Lists/MessageList.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList } from 'react-native';
 import { responsiveHeight } from 'react-native-responsive-dimensions';
 import { useGlobal } from 'reactn';
 import { gutters } from 'src/styles';
@@ -9,43 +9,56 @@ export default function MessageList({
   loadMoreMsg,
   MessagesConfig,
   loadingComponent = null,
+  style,
+  contentContainerStyle,
 }) {
   const [uid] = useGlobal('uid');
   const flatListRef = useRef();
 
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        ref={flatListRef}
-        inverted={true}
-        onLayout={() =>
-          setTimeout(() => flatListRef.current?.scrollToOffset({ y: 0 }), 300)
+    <FlatList
+      ref={flatListRef}
+      inverted={true}
+      onLayout={() =>
+        setTimeout(() => flatListRef.current?.scrollToOffset({ y: 0 }), 300)
+      }
+      ListFooterComponent={() => loadingComponent && loadingComponent()}
+      onEndReached={() => loadMoreMsg()}
+      onEndReachedThreshold={0.5}
+      style={style}
+      contentContainerStyle={{
+        paddingBottom: responsiveHeight(2),
+        paddingHorizontal: gutters,
+        flexGrow: 1,
+        justifyContent: 'flex-end',
+        ...contentContainerStyle,
+      }}
+      keyExtractor={(item, index) => index.toString()}
+      data={messages}
+      renderItem={({ item, index }) => {
+        const { sender, type } = item;
+        const prevSenderIsSame =
+          messages[index - 1]?.sender === messages[index]?.sender;
+        const nextSenderIsSame =
+          messages[index + 1]?.sender === messages[index]?.sender;
+        const MessageComponent =
+          MessagesConfig?.find((conf) => conf?.type === type)?.component ||
+          null;
+        if (!MessageComponent) {
+          console.log(`Message type ${type} not found in config array`);
+          return null;
+        } else {
+          return (
+            <MessageComponent
+              prevSenderIsSame={prevSenderIsSame}
+              nextSenderIsSame={nextSenderIsSame}
+              isMyMessage={sender === uid}
+              {...item}
+            />
+          );
         }
-        ListFooterComponent={() => loadingComponent && loadingComponent()}
-        onEndReached={() => loadMoreMsg()}
-        onEndReachedThreshold={0.5}
-        contentContainerStyle={{
-          paddingBottom: responsiveHeight(2),
-          paddingHorizontal: gutters,
-          flexGrow: 1,
-          justifyContent: 'flex-end',
-        }}
-        keyExtractor={(item, index) => index.toString()}
-        data={messages}
-        renderItem={({ item }) => {
-          const { sender, type } = item;
-          const MessageComponent =
-            MessagesConfig?.find((conf) => conf?.type === type)?.component ||
-            null;
-          if (!MessageComponent) {
-            console.log(`Message type ${type} not found in config array`);
-            return null;
-          } else {
-            return <MessageComponent isMyMessage={sender === uid} {...item} />;
-          }
-        }}
-        extraData={messages}
-      />
-    </View>
+      }}
+      extraData={messages}
+    />
   );
 }
