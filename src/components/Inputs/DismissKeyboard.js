21b64b7634fac627eb7ba4860da03c7ac31e8d21
Théo Hudry <theo@agenceminuit.com>
2023-07-29 15:46:34 +0200

    no keyboard dismiss on web
    Signed-off-by: Théo Hudry <theo@agenceminuit.com>

--- src/components/Inputs/DismissKeyboard.js
+++ src/components/Inputs/DismissKeyboard.js
@@ -1,7 +1,16 @@
 import React from 'reactn';
-import { View, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import {
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Platform,
+} from "react-native";
 
 export default function DismissKeyboard({ children }) {
+  if (Platform.OS === "web") {
+    return <View style={{ flex: 1 }}>{children}</View>;
+  }
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={{ flex: 1 }}>{children}</View>
