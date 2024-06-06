05d8dad22570019f9bd5b104f3d34264e4e87798
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-06 18:52:01 +0000

    edit_file

--- src/providers/index.js
+++ src/providers/index.js
@@ -2,5 +2,4 @@ import TooltipProvider from "./TooltipProvider";
 import LoadingProvider from "./LoadingProvider";
 import ShakeProvider from "./ShakeProvider";
 import ConsoleLogProvider from "./ConsoleLogProvider";
-
 export { TooltipProvider, LoadingProvider, ShakeProvider, ConsoleLogProvider };
