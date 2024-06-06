829295018b1c886b6fcac1fcfd8035dbf6c29086
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-06 18:51:36 +0000

    edit_file

--- src/styles/index.js
+++ src/styles/index.js
@@ -2,5 +2,4 @@ import Colors from './Colors';
 import Palette from './Palette';
 import Fonts from './Fonts';
 import SharedStyles, { gutters } from './SharedStyles';
-
 export { Colors, Palette, Fonts, SharedStyles, gutters };
